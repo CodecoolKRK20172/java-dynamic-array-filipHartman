f81beebee644ea20272ad68c24bd56a94bc3ba15
filipHartman <filip.hartman23@gmail.com>
2018-05-14 21:49:18 +0200

    Implement method to add element at the end of array

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -6,15 +6,28 @@ public class DynamicIntArray {
     public static final int DEFAULT_ARRAY_SIZE = 10;
 
     private int[] array;
+    private int size;
 
     public DynamicIntArray() {
         this.array = new int[DEFAULT_ARRAY_SIZE];
+        this.size = 0;
     }
-    public DynamicIntArray(int size) {
-        this.array = new int[size];
+    public DynamicIntArray(int arraySize) {
+        this.array = new int[arraySize];
+        this.size = 0;
     }
 
     public void add(int element) {
+        if(size < array.length) {
+            array[size++] = element;
+        } else {
+            int[] temp = array;
+            extendArray();
+            for (int tempElement : temp) {
+                add(tempElement);
+            }
+            add(element);
+        }
 
     }
 
@@ -24,5 +37,19 @@ public class DynamicIntArray {
 
     public void insert(int index, int element) {
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int element: array) {
+            sb.append(" " + element);
+        }
+        return sb.toString();
+    }
+
+    private void extendArray() {
+        this.array = new int[array.length + 1];
+        this.size = 0;
     }
 }
