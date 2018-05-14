32e75ee9862e20a67526f23123124217cc51fdec
filipHartman <filip.hartman23@gmail.com>
2018-05-14 21:14:28 +0200

    Add signatures of methods to fail tests

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,28 @@
 package com.codecool.dynamicArrayDojo;
 
+
 // put your code here!
 public class DynamicIntArray {
+    public static final int DEFAULT_ARRAY_SIZE = 10;
 
+    private int[] array;
+
+    public DynamicIntArray() {
+        this.array = new int[DEFAULT_ARRAY_SIZE];
+    }
+    public DynamicIntArray(int size) {
+        this.array = new int[size];
+    }
+
+    public void add(int element) {
+
+    }
+
+    public void remove(int element) {
+
+    }
+
+    public void insert(int index, int element) {
+
+    }
 }
